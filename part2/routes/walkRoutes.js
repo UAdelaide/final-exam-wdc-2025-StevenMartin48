89724147388c2a7c64f5eb7255f469a063406306
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:19:33 +0000

    [automatic commit] 2025-06-20 13:19:33 UTC 45.249.116.229

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -80,7 +80,7 @@ router.post('/login', async (req, res) =>{
 
  const [databaseUserData] = await db.query('SELECT * FROM Users WHERE Username = ?', [providedCredentials.username]);
 
- if(databaseUserData.Length !== 0) {return res.status(401).send('Username not found'); } // checks if the above query is empty. Is only empty if user doesn't exist.
+ if(databaseUserData.Length !== 0) { return res.status(401).send('Username not found'); } // checks if the above query is empty. Is only empty if user doesn't exist.
 
  if(providedCredentials.password === databaseUserData[0].password_hash){ // [0] because
 
