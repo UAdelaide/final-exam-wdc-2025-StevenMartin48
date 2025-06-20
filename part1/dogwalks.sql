b1f7b368a079436ebc7f56f7c0b20b22d5b2ae93
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 07:05:49 +0000

    [automatic commit] 2025-06-20 07:05:49 UTC 45.249.116.229

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -67,7 +67,7 @@ INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'JEFF'), 'Godzilla', 'small');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT user_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open');
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT user_id FROM Dogs WHERE name = ''), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted');
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (3, '2025-06-10 10:30:00', 60, 'The park', 'accepted');
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT user_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted');
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES ((SELECT user_id FROM Dogs WHERE name = 'mrBig'), '2025-06-10 10:30:00', 60, 'The park', 'accepted');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (4, '2025-06-01 10:30:00', 60, 'Space', 'cancelled');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (5, '2025-06-01 12:30:00', 180, 'The Beach', 'completed');
