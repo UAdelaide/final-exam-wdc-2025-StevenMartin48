a3c7e3fbcee14ff13360654a42f27ba876f9ffd9
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 16:27:36 +0000

    [automatic commit] 2025-06-20 16:27:36 UTC 45.249.116.229

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 31, // cookies age, 
+    maxAge: 1000 * 60 * 60 * 24 * 31, // cookies age, ms, seconds, hours, days
     secure: false
    }
   }
