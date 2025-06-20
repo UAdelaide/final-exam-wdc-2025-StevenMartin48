b7fc961d4fceda2fbc0331aa549f4bb124b90dd1
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:25:20 +0000

    [automatic commit] 2025-06-20 06:25:20 UTC 45.249.116.229

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -56,4 +56,5 @@ CREATE TABLE WalkRatings (
 
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
-INSERT INTO Users (username, email, password_hash, role) VALUES ('carol123', 'carol@example.com', 'hashed789', 'walker');
+INSERT INTO Users (username, email, password_hash, role) VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner');
+INSERT INTO Users (username, email, password_hash, role) VALUES ('JEFF', 'JEFF@example.com', 'hashed456', 'walker');
