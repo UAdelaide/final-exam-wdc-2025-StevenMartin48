f81fd702c1741a48e1a45324b6447483db13e982
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 07:45:38 +0000

    [automatic commit] 2025-06-20 07:45:38 UTC 45.249.116.229

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -11,7 +11,7 @@ host: 'localhost',
 user: 'root'
 });
 
-await connection.query('')
+await connection.query('CREATE DATABSE IF NOT EXISTS ')
 
 console.log('Mysql Connection established');
   } catch (err) {console.log('connection failed \n', err); } // only reason for this to fail is database not existing
