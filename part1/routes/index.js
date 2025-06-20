b54db824308bcef7a55902e2294424014d475067
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 08:51:33 +0000

    [automatic commit] 2025-06-20 08:51:33 UTC 45.249.116.229

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -144,12 +144,12 @@ router.get('/api/walkrequests/open', async(req, res, next) => {
  }
 });
 
-router.get('/api/walkrequests/open', async(req, res, next) => {
+router.get('/api/walkers/summary', async(req, res, next) => {
   try{
   const [walkerSummary] = await database.execute(`
     SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE WalkRequests.status = 'open';
     `);
-  res.status(200).send(openrequests);
+  res.status(200).send(walkerSummary);
  } catch (err){
   console.log(err);
   res.status(500).send('Error');
