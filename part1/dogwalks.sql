e93a2adb43a38a74b7647db00f76e518343e626e
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:42:24 +0000

    [automatic commit] 2025-06-20 06:42:24 UTC 45.249.116.229

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -66,4 +66,7 @@ INSERT INTO Dogs (owner_id, name, size) VALUES (4, 'Maximillian', 'large');
 INSERT INTO Dogs (owner_id, name, size) VALUES (1, 'Supermax', 'large');
 INSERT INTO Dogs (owner_id, name, size) VALUES (4, 'Godzilla', 'small');
 
+A request for Max at 2025-06-10 08:00:00 for 30 minutes at Parklands, with status open.
+A request for Bella at 2025-06-10 09:30:00 for 45 minutes at Beachside Ave, with status accepted.
+
 INSERT INTO WalkRequests ()
