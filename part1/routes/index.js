63c6c049d7960f1e06ce692cc0008e2d7b9e737f
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 08:17:45 +0000

    [automatic commit] 2025-06-20 08:17:45 UTC 45.249.116.229

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -89,13 +89,9 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('JEFF', 'JEFF@jeffdabest.org', 'hashedforalltime', 'owner'),('johnwalker', 'whiskey@times.com', 'hashedandsalted', 'walker');
-INSERT INTO Users (username, email, password_hash, role) VALUES ;
-INSERT INTO Users (username, email, password_hash, role) VALUES ;
-INSERT INTO Users (username, email, password_hash, role) VALUES ;
-INSERT INTO Users (username, email, password_hash, role) VALUES ;
 
-INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium');
-INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small');
+INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'JEFF'), 'mrBig', 'large');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Supermax', 'large');
 INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'JEFF'), 'Godzilla', 'small');
