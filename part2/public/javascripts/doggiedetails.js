06a8fcf0f6c2ae64ebe55c45ce66ce1bfe9bd2d2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 03:02:53 +0000

    [automatic commit] 2025-06-21 03:02:53 UTC 45.249.116.229

--- part2/public/javascripts/doggiedetails.js
+++ part2/public/javascripts/doggiedetails.js
@@ -2,16 +2,3 @@
 var doggies = {};
 
 
-function getdogs(){
-
-const xhttp = new XMLHttpRequest();
-xhttp.onload = function(){
-
-doggies = this.response;
-
-
-};
-xhttp.open("get", "/getownerdogs");
-xhttp.send();
-
-}
